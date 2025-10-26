cbdf8eb68f9ce47c28b2b78382fe6dcc88959093
git_leasarajlic <leas>
2025-10-26 11:00:11 +0100

    popravljene funkcije i imena varijabli

--- Vjezba1/zadatak1.c
+++ Vjezba1/zadatak1.c
@@ -5,38 +5,31 @@
 //struktura za spremanje podataka o studentima
 typedef struct Student
 {
-	char ime[20], prez[20];
-	int apsolutni_bodovi; //bodovi ostvareni na ispitu(iz datoteke)
-	float relativni_bodovi; //bodovi studenta u odnosu na max broj bodova
+	char name[20], last_name[20];
+	int apsolute_points; //bodovi ostvareni na ispitu(iz datoteke)
+	float relative_points; //bodovi studenta u odnosu na max broj bodova
 }_stud;
 
 //protipovi funkcija
-int brojac_redova(FILE*); 
-_stud* ucitaj_podatke(FILE*, int);
-int relativni_bodovi(_stud*, int);
-int ispis(_stud*, int);
+int GetLines(); 
+_stud* GetFileData(int);
+int GetRelativePts(_stud*, int);
+int Print(_stud*, int);
 
 int main()
 {
 	_stud* s;
 	int br_redova;
-	FILE* readFile;
-
-	//otvaranje datoteke
-	readFile = fopen("StudentiPrimjer.TXT", "r");
-	if (readFile == NULL) {
-		printf("Greska pri otvaranju datoteke!\n");
-		return 1;
-	}
+	
 
 	//racuna broj redova(studenata) u datoteci
-	br_redova = brojac_redova(readFile);
+	br_redova = GetLines();
 
 	//cita podatke iz datoteke
-	s = ucitaj_podatke(readFile, br_redova);
+	s = GetFileData(br_redova);
 
-	relativni_bodovi(s, br_redova);
-	ispis(s, br_redova);
+	GetRelativePts(s, br_redova);
+	Print(s, br_redova);
 
 	//oslobadanje memorije
 	free(s);
@@ -44,55 +37,69 @@ int main()
 	return 0;
 }
 
-int brojac_redova(FILE* read) {
+int GetLines() {
+	FILE* readFile;
+	//otvaranje datoteke
+	readFile = fopen("StudentiPrimjer.TXT", "r");
+	if (readFile == NULL) {
+		printf("Greska pri otvaranju datoteke!\n");
+		exit(EXIT_FAILURE);
+	}
 	int n = 0;
 	char c;
-	while ((c = fgetc(read)) != EOF) {
+	while ((c = fgetc(readFile)) != EOF) {
 		if(c == '\n')
 		n++;
 	}
-	rewind(read);  //pokazivac se vraca na pocetak datoteke
+	rewind(readFile);  //pokazivac se vraca na pocetak datoteke
 	return n + 1; //dodaje se 1 jer zadnji red obicno nema '\n'
 }
 
-_stud* ucitaj_podatke(FILE* read, int redovi)
-{
+_stud* GetFileData(int lines){
+	FILE* readFile;
+
+	//otvaranje datoteke
+	readFile = fopen("StudentiPrimjer.TXT", "r");
+	if (readFile == NULL) {
+		printf("Greska pri otvaranju datoteke!\n");
+		exit(EXIT_FAILURE);
+	}
 	//dinamicka alokacija memorije 
-	_stud* s = (_stud*)malloc(redovi * sizeof(_stud));
+	_stud* s = (_stud*)malloc(lines * sizeof(_stud));
 	if (s == NULL) {
 		printf("Greska pri alokaciji memorije!\n");
 		return 0;
 	}
-	for (int i = 0; i < redovi; i++) {
-		fscanf(read, "%s %s %d", s[i].ime, s[i].prez, &s[i].apsolutni_bodovi);
+	for (int i = 0; i < lines; i++) {
+		fscanf(readFile, "%s %s %d", s[i].name, s[i].last_name, &s[i].apsolute_points);
 
 	}
-	fclose(read);
+	fclose(readFile);
 	return s;
 }
 
-int relativni_bodovi(_stud* s, int redovi) 
+int GetRelativePts(_stud* s, int lines)
 {
-	int max_bodovi = s[0].apsolutni_bodovi;
+	int max_bodovi = s[0].apsolute_points;
 
 	//trazimo studenta s najvise bodova
-	for (int i = 0; i < redovi; i++) {
-		if (s[i].apsolutni_bodovi > max_bodovi)
-			max_bodovi = s[i].apsolutni_bodovi;
+	for (int i = 0; i < lines; i++) {
+		if (s[i].apsolute_points > max_bodovi)
+			max_bodovi = s[i].apsolute_points;
 	}
 
 	//racunamo relativne bodove za sve studente
-	for (int i = 0; i < redovi; i++) {
-		s[i].relativni_bodovi = ((float)s[i].apsolutni_bodovi / max_bodovi) * 100;
+	for (int i = 0; i < lines; i++) {
+		s[i].relative_points = ((float)s[i].apsolute_points / max_bodovi) * 100;
 	}
 	return 0;
 }
 
-int ispis(_stud* s, int redovi)
+int Print(_stud* s, int redovi)
 {
 	printf("Ime        Prezime      Apsolutni bodovi      Relativni bodovi(%%)\n\n");
 	for (int i = 0; i < redovi; i++) {
-		printf("%-10s %-20s %-15d %.2f%%\n", s[i].ime, s[i].prez, s[i].apsolutni_bodovi, s[i].relativni_bodovi);
+		printf("%-10s %-20s %-15d %.2f%%\n", s[i].name, s[i].last_name, s[i].apsolute_points, s[i].relative_points);
 	}
 	return 0;
 }
